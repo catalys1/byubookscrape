088ff74ce5e422d821dd6f57ecf77fedb1ed99ef
Connor <thecatalystak@gmail.com>
2017-12-22 11:04:37 -0700

    Some changes

--- byu_book_scrape.py
+++ byu_book_scrape.py
@@ -8,9 +8,12 @@ import time
 import numpy
 import re
 import json
+import random
+import os
+import getpass
 
 
-delay = 10
+delay = 12
 
 browser = webdriver.Chrome()
 browser.get("https://my.byu.edu")
@@ -21,8 +24,9 @@ browser.find_element_by_xpath('//a[@href="https://cas.byu.edu/cas/login?service=
 username = browser.find_element_by_id("netid")
 password = browser.find_element_by_id("password")
 
-username.send_keys("cvilorio")
-password.send_keys("byu12345")
+username.send_keys("catalys1")
+pwd = getpass.getpass()
+password.send_keys(pwd)
 
 browser.find_element_by_xpath('//input[@class="submit"]').click()
 time.sleep(delay)
@@ -53,8 +57,8 @@ def process_page():
 			options = amazon.find_elements_by_xpath('.//option')
 			for o in options:
 				o.click()
-				time.sleep(1)
-				price = amazon.find_elements_by_xpath('.//span[contains(@data-bind,"text: price")]')
+				time.sleep(random.randint(1,2))
+				price = amazon.find_element_by_xpath('.//span[contains(@data-bind,"text: price")]')
 				if o.text == 'New':
 					BOOK['amazon_new'] = price.text
 				elif o.text == 'Used':
@@ -69,26 +73,35 @@ departments = department_selector.find_elements_by_xpath('.//option')
 
 out_file = 'scrape.json'
 results = {}
+if os.path.isfile(out_file):
+	results = json.load(open(out_file))
 print 'Starting departments'
 for dept in departments[1:]:
+	dept_name = dept.text
+	if dept_name in results: continue
 	dept.click()
 	classes = dept.find_elements_by_xpath('//select')[-1]
 	classes = classes.find_elements_by_xpath('.//option')
 	class_dict = {}
 	print dept.text
-	for cls in classes[1:]:
-		print '  {}'.format(cls.text)
+	for i in xrange(1, len(classes)):
+		cls = classes[i]
+		class_name = cls.text
+		print '  {}'.format(class_name)
 		cls.click()
-		time.sleep(4)
+		time.sleep(random.randint(5,6))
+		#import pdb; pdb.set_trace()
 		browser.find_element_by_xpath("//input[@type='text']").click()
-		time.sleep(1)
+		time.sleep(random.randint(1,2))
 		browser.find_element_by_xpath("//input[@type='checkbox']").click()
-		time.sleep(3)
+		time.sleep(random.randint(3,6))
 		books = process_page()
-		class_dict[cls.text] = books
-	results[dept.text] = class_dict
+		class_dict[class_name] = books
+		classes = dept.find_elements_by_xpath('//select')[-1]
+		classes = classes.find_elements_by_xpath('.//option')
+	results[dept_name] = class_dict
 	json.dump(results, open(out_file, 'w'))
-	time.sleep(5)
+	time.sleep(random.randint(20,30))
 
 
 
